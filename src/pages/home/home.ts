f7a8abf6801aa549266af11e668b2b5f21d35629
Paolo Escobar <paolo.escobar@hubesco.com>
2017-12-20 17:00:20 +0000

    As a walker, I want to see the list of dog walkers around me on my home page in order to see if I can connect with other people - Reproduce sketch in mobile app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,40 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  dogsAround: Array<Dog>;
 
+  constructor(public navCtrl: NavController) {
+    this.loadDogs();
   }
 
+  private loadDogs() {
+    this.dogsAround = new Array<Dog>();
+    let hyumiko: Dog = new Dog();
+    hyumiko.uuid = "1";
+    hyumiko.name = "Hyumiko";
+    hyumiko.breed = "Shiba Inu"
+    hyumiko.rate = 5.0;
+    hyumiko.imgUrl = "assets/imgs/avatar-shiba.jpg"
+    this.dogsAround.push(hyumiko);
+    let reveur: Dog = new Dog();
+    reveur.uuid = "2";
+    reveur.name = "Reveur";
+    reveur.breed = "Teckel"
+    reveur.rate = 5.0;
+    reveur.imgUrl = "assets/imgs/avatar-teckel.jpg";
+    this.dogsAround.push(reveur);
+  }
+
+  viewDog(uuid) {
+    console.log(uuid);
+  }
+
+}
+
+class Dog {
+  uuid: string;
+  name: string;
+  breed: string;
+  rate: number;
+  imgUrl: string;
 }
